60fdb4ab4efb6f0ad10907970d28223dac9fa7b6
VladSkopenko <skopirka2k17@gmail.com>
2024-04-09 04:32:05 +0300

    Bot reminder complete

---
+++ bot.py
@@ -0,0 +1,47 @@
+import time
+import logging
+import os
+import asyncio
+import sys
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ.get("TOKEN")
+
+dp = Dispatcher()
+MSG = "Чи завантажував ти свій код  на гітхаб сьогодні, {}?"
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    user_id = message.from_user.id
+    user_fullname = message.from_user.full_name
+    logging.info(f"{user_id=} {user_fullname=}, {time.asctime()}")
+    await message.reply(f"Привіт ,{user_fullname}")
+
+
+@dp.message()
+async def remind_git(message: Message):
+    user_name = message.from_user.first_name
+
+    for _ in range(20):
+        time.sleep(2)
+
+        await message.answer(MSG.format(user_name))
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
