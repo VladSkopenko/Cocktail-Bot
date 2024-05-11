70178ad780ccc5924c6b0511a96c0504b95e9d9f
VladSkopenko <skopirka2k17@gmail.com>
2024-05-11 15:39:06 +0300

    fix models

--- src/database/models.py
+++ src/database/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from sqlalchemy import DateTime
 from sqlalchemy import Enum
+from sqlalchemy import Float
 from sqlalchemy import ForeignKey
 from sqlalchemy import func
 from sqlalchemy import Integer
@@ -25,25 +26,25 @@ class Alcohol(enum.Enum):
 
 class Taste(Base):
     __tablename__ = "tastes"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     taste: Mapped[str] = mapped_column(String(60), nullable=False)
 
 
 class Coctail(Base):
     __tablename__ = "coctails"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
     image: Mapped[str] = mapped_column(String(255), nullable=False)
-    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
     author: Mapped[str] = mapped_column(String(100), nullable=False)
-    alko: Mapped[Enum] = mapped_column("tag_type", Enum(Alcohol), nullable=False)
+    alcohol_level: Mapped[Enum] = mapped_column("tag_type", Enum(Alcohol), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
 
 
 class TasteCoctail(Base):
     __tablename__ = "taste_coctails"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     taste_id: Mapped[int] = mapped_column(ForeignKey("tastes.id"), nullable=False)
     coctail_id: Mapped[int] = mapped_column(ForeignKey("coctails.id"), nullable=False)
