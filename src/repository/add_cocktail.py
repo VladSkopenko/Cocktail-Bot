87942813678010df6c22dfecee411cbb64309ed2
VladSkopenko <skopirka2k17@gmail.com>
2024-05-12 23:08:21 +0300

    Ad get_cockt

--- src/repository/add_cocktail.py
+++ src/repository/add_cocktail.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy import update
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models import Cocktail
@@ -12,3 +15,24 @@ async def repository_add_cocktail(session: AsyncSession, data: dict):
     )
     session.add(cocktail)
     await session.commit()
+
+
+async def repository_get_all_cocktails(session: AsyncSession):
+    query = select(Cocktail)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def repository_get_cocktail_by_id(
+    session: AsyncSession, cocktail_key: int, mode: str = "by_id"
+):
+    if mode == "by_id":
+        query = select(Cocktail).where(Cocktail.id == cocktail_key)
+    elif mode == "by_name":
+        query = select(Cocktail).where(Cocktail.name == cocktail_key)
+    else:
+        raise ValueError(
+            "Непідтримуваний режим. Режим повинен бути 'by_id' або 'by_name."
+        )
+    result = await session.execute(query)
+    return result.scalar_one_or_none()
