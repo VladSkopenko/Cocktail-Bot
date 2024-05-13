3081680d13a014df2c1f8315a626cc399981dbdb
VladSkopenko <skopirka2k17@gmail.com>
2024-05-14 00:26:53 +0300

    + user

---
+++ src/repository/repo_category.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.models import Category
+
+
+async def orm_get_categories(session: AsyncSession):
+    query = select(Category)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+async def orm_create_categories(session: AsyncSession, categories: list):
+    query = select(Category)
+    result = await session.execute(query)
+    if result.first():
+        return
+    session.add_all([Category(name=name) for name in categories])
+    await session.commit()
