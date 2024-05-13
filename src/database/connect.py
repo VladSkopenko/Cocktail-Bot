122d3532a3d9eab0676f2d69d2f2638b0899201a
VladSkopenko <skopirka2k17@gmail.com>
2024-05-14 00:48:10 +0300

    + cater and banner

--- src/database/connect.py
+++ src/database/connect.py
@@ -2,6 +2,9 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from src.database.models import Base
+from src.common.text_for_db import categories, description_for_info_pages
+from src.repository.repo_banner import repository_add_banner_description
+from src.repository.repo_category import repository_create_categories
 
 db_url = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 engine = create_async_engine(db_url, echo=True)
@@ -10,10 +13,13 @@ session_maker = async_sessionmaker(
 )
 
 
-
 async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        await repository_create_categories(session, categories)
+        await repository_add_banner_description(session, description_for_info_pages)
 
 
 async def drop_db():
